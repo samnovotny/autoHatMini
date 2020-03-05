15fdaac5c3d76f94f9f92727b19077a1f6d9a608
Sam <sam@samnovotny.com>
2020-03-05 21:19:03 +0000

    Added comments.

--- Sources/autoHatMini/I2CIo.swift
+++ Sources/autoHatMini/I2CIo.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-
 enum I2CError : Error {
     case ioctlError
     case writeError
     case readError
 }
 
+/// Description
 class I2CIo {
     
     let fd: Int32
@@ -30,18 +30,26 @@ class I2CIo {
         close(self.fd)
     }
     
+    /// Select the I2c slave for forthoming trasition.
+    
     private func selectDevice() throws {
         let io = ioctl(self.fd, UInt(I2C_SLAVE), CInt(self.address))
         guard io != -1 else {throw I2CError.ioctlError}
     }
     
+    /// Create mask to set the ADC
+    /// - Parameter ch: The channel (1-3) indexed (0-2) to generate the mask for
+
     func config(ch: Int) -> UInt16 {
         let channels: [UInt16] = [0x4000, 0x5000, 0x6000]
         let config: UInt16 = 0x0003 | 0x0100 | 0x0080 | 0x0200 | 0x8000 | channels[ch]
 
         return(config)
     }
-        
+    
+    /// Read the ADC for the given channel
+    /// - Parameter channel: select the channel to run the ADC for
+    
     func readADC(channel: Int) throws -> Float {
         print ("\(#function) - \(channel)", terminator: " ")
  
